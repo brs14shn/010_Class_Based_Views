90fd40d57c1c338c2f9fdf123d0ec22b06a7891f
brs14shn <brsshn292929@gmail.com>
2022-08-27 12:31:23 +0300

    updated

--- fscohort/urls.py
+++ fscohort/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from .views import home,student_list, student_add, student_detail, student_update,student_delete
-
+from .views import home, student_list, student_add, student_detail, student_update, student_delete
+from .views import HomeView
 urlpatterns = [
-    path('', home, name="home"),
+    # * FUNCTİON BASED
+    # path('', home, name="home"),
     path('student_list/', student_list, name="list"),
     path('student_add/', student_add, name="add"),
     path('detail/<int:id>/', student_detail, name="detail"),
     path('update/<int:id>/', student_update, name="update"),
     path('delete/<int:id>/', student_delete, name="delete"),
+
+    # * CLASS BASED
+    path("", HomeView.as_view(), name="home"),
 ]
