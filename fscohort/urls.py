a022df329a3f16933044c6aa25326134a6ff31b5
brs14shn <brsshn292929@gmail.com>
2022-08-27 13:40:37 +0300

    updated

--- fscohort/urls.py
+++ fscohort/urls.py
@@ -1,17 +1,21 @@
 from django.urls import path
 from .views import home, student_list, student_add, student_detail, student_update, student_delete
-from .views import HomeView, StudentListView, StudentDetailView
+from .views import HomeView, StudentListView, StudentDetailView, StudentCreateView, Student_Update_View, StudentDelete
 urlpatterns = [
     # * FUNCTİON BASED
     # path('', home, name="home"),
     # path('student_list/', student_list, name="list"),
-    path('student_add/', student_add, name="add"),
-    path('detail/<int:id>/', student_detail, name="detail"),
-    path('update/<int:id>/', student_update, name="update"),
+    # path('student_add/', student_add, name="add"),
+    # path('detail/<int:id>/', student_detail, name="detail"),
+    # path('update/<int:id>/', student_update, name="update"),
     path('delete/<int:id>/', student_delete, name="delete"),
 
     # * CLASS BASED
     path("", HomeView.as_view(), name="home"),
     path("student_list/", StudentListView.as_view(), name="list"),
     path("detail/<int:id>/", StudentDetailView.as_view(), name="detail"),
+    #! detail/<int:pk>/ olarak belirtirsek views tanımlamaya gerek kalmazdı
+    path("student_add/", StudentCreateView.as_view(), name="add"),
+    path("update/<int:pk>/", Student_Update_View.as_view(), name="update"),
+    path("delete/<int:pk>", StudentDelete.as_view(), name="delete")
 ]
--- fscohort/views.py
+++ fscohort/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .forms import StudentForm
 from .models import Student
+from django.urls import reverse_lazy
+
 # Create your views here.
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
 # ?====FUNCTİON BASED==========================
 
@@ -27,29 +29,33 @@ def student_list(request):
     }
     return render(request, "fscohort/student_list.html", context)
 
-
 #!======= CLASS BASED=========================
+
+
 class StudentListView(ListView):
     model = Student
     context_object_name = "students"
-    paginate_by = 1
+    # paginate_by = 1
 
 
 def student_add(request):
     form = StudentForm()
-
     if request.method == "POST":
         form = StudentForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect("list")
-
     context = {
-
         "form": form
     }
-
     return render(request, "fscohort/student_add.html", context)
+
+
+class StudentCreateView(CreateView):
+    model = Student
+    form_class = StudentForm
+    template_name = "fscohort/student_add.html"
+    success_url = reverse_lazy("list")
 
 
 def student_detail(request, id):
@@ -60,11 +66,13 @@ def student_detail(request, id):
 
     return render(request, "fscohort/student_detail.html", context)
 
+#!======= CLASS BASED=========================
+
 
 class StudentDetailView(DetailView):
     model = Student
     pk_url_kwarg = 'id'
-    # Default pk olarak url kısmında belirtirsek burada belirtmeye gerek yok
+    # * Default **pk** olarak url kısmında belirtirsek burada belirtmeye gerek yok
 
 
 def student_update(request, id):
@@ -88,6 +96,13 @@ def student_update(request, id):
     return render(request, "fscohort/student_update.html", context)
 
 
+class Student_Update_View(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = "fscohort/student_update.html"
+    success_url = reverse_lazy("list")
+
+
 def student_delete(request, id):
 
     student = Student.objects.get(id=id)
@@ -101,3 +116,9 @@ def student_delete(request, id):
         "student": student
     }
     return render(request, "fscohort/student_delete.html", context)
+
+
+class StudentDelete(DeleteView):
+    model = Student
+    template_name = "fscohort/student_delete.html"
+    success_url = reverse_lazy("list")
